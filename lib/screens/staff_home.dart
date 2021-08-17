fe6fb4ba74a3db76ab44d01a2ebc2f2a74086f60
Vamsi Sai Garapati <35292779+vamsisaigarapati@users.noreply.github.com>
2021-08-17 13:14:45 +0530

    change staff_home to quiz entry to check merge possibility

--- lib/screens/staff_home.dart
+++ lib/screens/staff_home.dart
@@ -1,134 +1,400 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import '../modals/feed_class.dart';
-import '../providers/feed_provider.dart';
-import 'package:intl/intl.dart';
 import '../drawer/collapsing_navigation_drawer_widget.dart';
-import './feed_upload.dart';
-import './making_announcement.dart';
+import 'package:intl/intl.dart';
+import '../modals/quiz.dart';
+import 'package:provider/provider.dart';
+import '../providers/quiz_entry_provider.dart';
+import './question_entry_screen.dart';
+import '../providers/student_provider.dart';
 
-class StaffHome extends StatefulWidget {
-  static const routeName = '/staff-home';
+class QuizDetailsEntryScreen extends StatefulWidget {
+  static const routeName = 'quiz-entry';
   @override
-  _StaffHomeState createState() => _StaffHomeState();
+  _QuizDetailsEntryScreenState createState() => _QuizDetailsEntryScreenState();
 }
 
-class _StaffHomeState extends State<StaffHome> {
-  List<Feed> _totalFeed = [];
+class _QuizDetailsEntryScreenState extends State<QuizDetailsEntryScreen> {
+  final _subjectFocusNode = FocusNode();
+  final _noOfQuestionsFocusNode = FocusNode();
 
-  Future<void> _refreshFeed(BuildContext context) async {
-    await Provider.of<FeedProvider>(context,listen: false).fetchNewsFeed().then((_) {
-      print("hellllo Namasthe");
-      _totalFeed = Provider.of<FeedProvider>(context,listen: false).items;
+  final _departmentFocusNode = FocusNode();
+  final _form = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    _subjectFocusNode.dispose();
+    _noOfQuestionsFocusNode.dispose();
+    _departmentFocusNode.dispose();
+    super.dispose();
+  }
+
+  var i = 0;
+  var j = 0;
+  var _newDetails = QuizClass(
+      dateTime: DateTime.now(),
+      department: '',
+      year: 0,
+      subject: '',
+      noOfQuestions: 0);
+
+  DateTime _submittedDate;
+  TimeOfDay _submittedTime;
+  var _isLoading = false;
+  void _quizTimePicker() {
+    showTimePicker(
+      context: context,
+      initialTime: TimeOfDay.now(),
+    ).then((pickedTime) {
+      if (pickedTime == null) {
+        return;
+      }
+      setState(() {
+        _submittedTime = pickedTime;
+        final now = new DateTime.now();
+        final DateTime finalDateAndTime = DateTime(
+            _submittedDate.year,
+            _submittedDate.month,
+            _submittedDate.day,
+            pickedTime.hour,
+            pickedTime.minute);
+        _newDetails = QuizClass(
+            dateTime: finalDateAndTime,
+            department: _newDetails.department,
+            year: _newDetails.year,
+            subject: _newDetails.subject,
+            noOfQuestions: _newDetails.noOfQuestions);
+      });
     });
   }
 
- 
+  void _presentDatePicker() {
+    showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2019),
+      lastDate: DateTime(2030),
+    ).then((pickedDate) {
+      if (pickedDate == null) {
+        return;
+      }
+      setState(() {
+        _submittedDate = pickedDate;
+        _newDetails = QuizClass(
+            dateTime: pickedDate,
+            department: _newDetails.department,
+            year: _newDetails.year,
+            subject: _newDetails.subject,
+            noOfQuestions: _newDetails.noOfQuestions);
+      });
+    });
+  }
 
-  Widget buildEachFeed(Feed data) {
-    return Card(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(15),
-      ),
-      elevation: 6,
-      margin: EdgeInsets.all(10),
-      child: Column(
-        
-        children: <Widget>[
-          Padding(
-            padding: EdgeInsets.all(15),
-            child: ListTile(
-              title: Text(data.uploader.name),
-              subtitle: Text(data.uploader.department),
-              trailing: Text(
-                DateFormat.yMEd().format(data.time) +
-                    '\n' +
-                    'at ' +
-                    DateFormat.Hm().format(data.time),
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w300),
-              ),
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.all(15),
-            child: Container(
-                alignment: Alignment.topLeft,
-                child: Text(
-                  data.description,
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.normal),
-                )),
-          ),
-          Stack(
-            children: <Widget>[
-              ClipRRect(
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(15),
-                  topRight: Radius.circular(15),
-                ),
-                child: FadeInImage(
-                  placeholder: AssetImage('assets/images/feed_placeholder.jpg'),
-                  image:NetworkImage( data.imageUrl),
-                  height: 250,
-                  width: double.infinity,
-                  fit: BoxFit.cover,
-                ),
-              ),
+  int y = 0;
+  Future<void> _saveForm() async {
+    final isValid = _form.currentState.validate();
+    if (!isValid) {
+      return;
+    }
+    _form.currentState.save();
+    setState(() {
+      _isLoading = true;
+    });
+
+    if (_newDetails != null) {
+      try {
+        await Provider.of<Students>(context)
+            .fetchStudentsOnDeptAndYear(_newDetails);
+        await Provider.of<QuizEntry>(context, listen: false)
+            .addQuizEntry(_newDetails);
+      } catch (error) {
+        y = 1;
+        await showDialog(
+          context: context,
+          builder: (ctx) => AlertDialog(
+            title: Text('An error occurred!'),
+            content: Text('Something went wrong.'),
+            actions: <Widget>[
+              FlatButton(
+                child: Text('Okay'),
+                onPressed: () {
+                  Navigator.of(ctx).pop();
+                },
+              )
             ],
           ),
-        ],
-      ),
-    );
+        );
+      }
+    }
+
+    setState(() {
+      _isLoading = false;
+    });
+    if (y == 1) {
+      Navigator.of(context).pushReplacementNamed('/staff-home');
+    } else {
+      Navigator.of(context).pushReplacementNamed(QuestionEntryScreen.routeName,
+          arguments: _newDetails);
+    }
   }
+
+  int yearDropValue = 1;
+  String departmentDropValue = 'IT';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-       drawer: CollapsingNavigationDrawer(),
-        appBar: AppBar(
-          title: Text('Feed'),
-        ),
-        body: RefreshIndicator(
-          onRefresh: ()=>_refreshFeed(context),
-                  child: FutureBuilder(
-              future: _refreshFeed(context),
-              builder: (ctx, snapshot) =>
-                  snapshot.connectionState == ConnectionState.waiting
-                      ? Center(
-                          child: CircularProgressIndicator(),
-                        )
-                      :  ListView.builder(
-                              itemBuilder: (_, i) {
-                                return buildEachFeed(_totalFeed[_totalFeed.length-i-1]);
-                              },
-                              itemCount: _totalFeed.length,
+        drawer: CollapsingNavigationDrawer(),
+      appBar: AppBar(
+        title: Text('Quiz Details'),
+      ),
+      body: _isLoading
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Form(
+                key: _form,
+                child: ListView(
+                  children: <Widget>[
+                    Container(
+                      height: 70,
+                      child: Row(
+                        children: <Widget>[
+                          Expanded(
+                            child: Text(_submittedDate == null
+                                ? 'NO date choosen'
+                                : 'Picked Date:  ${DateFormat.yMd().format(_submittedDate)}'),
+                          ),
+                          FlatButton(
+                            textColor: Theme.of(context).primaryColor,
+                            child: Text(
+                              'choose date',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
                             ),
-                      
-  ),
-        ),
-             floatingActionButtonLocation:
-              FloatingActionButtonLocation.centerDocked,
-          floatingActionButton: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                FloatingActionButton(
-                  heroTag: '+',
-                  onPressed: () {
-                    Navigator.of(context).pushNamed(MakeAnAnnouncementScreen.routeName);
-                  },
-                  child: Icon(Icons.add_alert),
+                            onPressed: _presentDatePicker,
+                          )
+                        ],
+                      ),
+                    ),
+                    Container(
+                      height: 70,
+                      child: Row(
+                        children: <Widget>[
+                          Expanded(
+                            child: Text(_submittedTime == null
+                                ? 'NO Time choosen'
+                                : 'Picked Time:  ${_submittedTime.format(context)}'),
+                          ),
+                          FlatButton(
+                            textColor: Theme.of(context).primaryColor,
+                            child: Text(
+                              'Pick Time to Take Quiz',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            onPressed: _quizTimePicker,
+                          )
+                        ],
+                      ),
+                    ),
+                    // TextFormField(
+                    //   initialValue: '',
+                    //   decoration: InputDecoration(labelText: 'Studying Year'),
+                    //   textInputAction: TextInputAction.next,
+                    //   keyboardType: TextInputType.number,
+                    //   onFieldSubmitted: (_) {
+                    //     FocusScope.of(context)
+                    //         .requestFocus(_departmentFocusNode);
+                    //   },
+                    //   validator: (value) {
+                    //     if (value.isEmpty) {
+                    //       return 'Please enter Year of Studying.';
+                    //     }
+                    //     if (double.tryParse(value) == null) {
+                    //       return 'Please enter a valid Year.';
+                    //     }
+                    //     if (double.parse(value) <= 0 ||
+                    //         double.parse(value) >= 5) {
+                    //       return 'Please enter a number less than or equal to 4.';
+                    //     }
+                    //     return null;
+                    //   },
+                    //   onSaved: (value) {
+                    //     _newDetails = QuizClass(
+                    //         dateTime: _newDetails.dateTime,
+                    //         department: _newDetails.department,
+                    //         year: int.parse(value),
+                    //         subject: _newDetails.subject,
+                    //         noOfQuestions: _newDetails.noOfQuestions);
+                    //   },
+                    // ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: <Widget>[
+                        Text('Studying Year'),
+                        SizedBox(width: 180),
+                        DropdownButton<int>(
+                          value: yearDropValue,
+                          icon: Icon(Icons.arrow_downward),
+                          iconSize: 24,
+                          elevation: 16,
+                          underline: Container(
+                            height: 2,
+                            color: Theme.of(context).primaryColor,
+                          ),
+                          onChanged: (int newValue) {
+                            setState(() {
+                              _newDetails = QuizClass(
+                                  dateTime: _newDetails.dateTime,
+                                  department: _newDetails.department,
+                                  year: newValue,
+                                  subject: _newDetails.subject,
+                                  noOfQuestions: _newDetails.noOfQuestions);
+                              i = 1;
+                              yearDropValue = newValue;
+                            });
+                          },
+                          items: <int>[1, 2, 3, 4]
+                              .map<DropdownMenuItem<int>>((int value) {
+                            return DropdownMenuItem<int>(
+                              value: value,
+                              child: Text(value.toString()),
+                            );
+                          }).toList(),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: <Widget>[
+                        Text('Department'),
+                        SizedBox(width: 180),
+                        DropdownButton<String>(
+                          value: departmentDropValue,
+                          icon: Icon(Icons.arrow_downward),
+                          iconSize: 24,
+                          elevation: 16,
+                          underline: Container(
+                            height: 2,
+                            color: Theme.of(context).primaryColor,
+                          ),
+                          onChanged: (String newValue) {
+                            setState(() {
+                              _newDetails = QuizClass(
+                                  dateTime: _newDetails.dateTime,
+                                  department: newValue,
+                                  year: _newDetails.year,
+                                  subject: _newDetails.subject,
+                                  noOfQuestions: _newDetails.noOfQuestions);
+                              j = 1;
+                              departmentDropValue = newValue;
+                            });
+                          },
+                          items: <String>[
+                            'IT',
+                            'CSE',
+                            'ECE',
+                            'EEE',
+                            'MECH',
+                            'CIVIL'
+                          ].map<DropdownMenuItem<String>>((String value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(value),
+                            );
+                          }).toList(),
+                        ),
+                      ],
+                    ),
+                    TextFormField(
+                      initialValue: '',
+                      decoration: InputDecoration(labelText: 'Subject '),
+                      textInputAction: TextInputAction.next,
+                      focusNode: _subjectFocusNode,
+                      onFieldSubmitted: (_) {
+                        FocusScope.of(context)
+                            .requestFocus(_noOfQuestionsFocusNode);
+                      },
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Please provide a correct value.';
+                        }
+                        return null;
+                      },
+                      onSaved: (value) {
+                        value=value.split(" ").join('_');
+                        _newDetails = QuizClass(
+                            dateTime: _newDetails.dateTime,
+                            department: _newDetails.department,
+                            year: _newDetails.year,
+                            subject: value,
+                            noOfQuestions: _newDetails.noOfQuestions);
+                      },
+                    ),
+                    TextFormField(
+                      initialValue: '',
+                      decoration: InputDecoration(labelText: 'No Of Questions'),
+                      textInputAction: TextInputAction.next,
+                      keyboardType: TextInputType.number,
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Please enter No of questions.';
+                        }
+                        if (double.tryParse(value) == null) {
+                          return 'Please enter a valid number.';
+                        }
+                        if (double.parse(value) <= 0) {
+                          return 'Please enter a number greater than 1.';
+                        }
+                        return null;
+                      },
+                      onSaved: (value) {
+                        _newDetails = QuizClass(
+                            dateTime: _newDetails.dateTime,
+                            department: _newDetails.department,
+                            year: _newDetails.year,
+                            subject: _newDetails.subject,
+                            noOfQuestions: int.parse(value));
+                      },
+                    ),
+
+                    FlatButton(
+                      child: Text('Submit'),
+                      onPressed: () {
+                        if (i != 1 || j != 1) {
+                          showDialog(
+                            context: context,
+                            builder: (ctx) => AlertDialog(
+                              title:
+                                  Text('You didn\'t select department or year'),
+                              content: Text('Please Select.'),
+                              actions: <Widget>[
+                                FlatButton(
+                                  child: Text('Okay'),
+                                  onPressed: () {
+                                    Navigator.of(ctx).pop();
+                                  },
+                                )
+                              ],
+                            ),
+                          );
+                        } else {
+                          _saveForm();
+                        }
+                      },
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 30.0, vertical: 4),
+                      materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                      textColor: Theme.of(context).primaryColor,
+                    ),
+                  ],
                 ),
-                FloatingActionButton(
-                  heroTag: '++',
-                  onPressed: () {
-                    Navigator.of(context).pushNamed(FeedUploadScreen.routeName);
-                  },
-                  child: Icon(Icons.add),
-                )
-              ],
+              ),
             ),
-          )
-        );
+    );
   }
 }
